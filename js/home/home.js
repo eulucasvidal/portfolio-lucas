24ae8abfc5b2cada77374344c00d9596daf6c121
Lucas Vidal <luccas.lima12@gmail.com>
2023-06-14 16:29:21 -0300

    adicionando efeito scroll

--- js/home/home.js
+++ js/home/home.js
@@ -15,5 +15,28 @@ function initScrollSuave (){
     link.addEventListener('click',scrollToSection);
     });
 };
-
 initScrollSuave();
+
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+      const windowMetade = window.innerHeight * 0.8;
+  
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = (sectionTop - windowMetade) < 0;
+          if(isSectionVisible)
+            section.classList.add('ativo');
+          else 
+            section.classList.remove('ativo');
+        })
+      }
+  
+      animaScroll();
+  
+      window.addEventListener('scroll', animaScroll);
+    }
+  }
+  initAnimacaoScroll();
